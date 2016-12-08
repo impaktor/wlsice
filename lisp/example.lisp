0a34dbcb8fdb0d510ce3b8fac8d88d0b35d78746
Karl F <karlfogel@gmail.com>
2016-12-08 12:50:10 +0100

    WIP Fix up lisp

--- lisp/example.lisp
+++ lisp/example.lisp
@@ -2,33 +2,59 @@
 
 (in-package :common-lisp-user)
 
-(defun f (t parameters)
+(asdf:operate 'asdf:load-op :gsll)
+
+;;; Path to trajectory data files
+(defparameter *data-path* "../data/")
+
+(defun f (time parameters)
   "The analytical function of t to fit parameters to"
   (let ((a (car parameters))
         (b (cadr parameters)))
-    (mapcar (lambda (x) (* a (expt x b))) t)))
+    (mapcar (lambda (x) (* a (expt x b))) time)))
 
-(defun df (t parameters)
-  "Gradient of analytical function of t. Return 2-element list of gradient in each point t for each of our two parameters a, b"
+(defun df (time parameters)
+  "Gradient of analytical function of t. Return 2-element list of gradient
+ in each point t for each of our two parameters a, b"
   (let ((a (car parameters))
         (b (cadr parameters)))
-    (list (mapcar (lambda (x) (expt x b)) t)
-          (mapcar (lambda (x) (* a (log x) (expt x b)))))))
+    (list (mapcar (lambda (x) (expt x b)) time)
+          (mapcar (lambda (x) (* a (log x) (expt x b))) time))))
 
-(defun d2f (t parameters)
+(defun d2f (time parameters)
   "Hessian matrix of analytical function to fit"
   (let ((a (car parameters))
         (b (cadr parameters)))
     ;; TODO!
-    (mapcar (lambda (x) 0) t)           ; 0,0 is all 0
-    (mapcar (lambda (x) (* (log x) (expt x b))) t)    ;0,1
-    (mapcar (lambda (x) (* (log x) (expt x b))) t)    ;1,0
-    (mapcar (lambda (x) (* a (expt (log x) 2) (expt x b))))  ;1,1
+    (mapcar (lambda (x) 0) time)           ; 0,0 is all 0
+    (mapcar (lambda (x) (* (log x) (expt x b))) time)    ;0,1
+    (mapcar (lambda (x) (* (log x) (expt x b))) time)    ;1,0
+    (mapcar (lambda (x) (* a (expt (log x) 2) (expt x b))) time)  ;1,1
     ))
 
-(defun read-in-data (path)
-  "Reads in two-column data from path, returns a suitable data structure, of time vector and matrix of trajectories")
 
-;; start-time 200
+(defun get-second-column (input-file)
+    "Extract the second column, as a list, from a two-column file with
+floats. Skipp the first line in the input-file, it's a comment."
+  (with-open-file (stream input-file)
+    (read-line stream)                ; first line is a comment, skip it
+    (do ((line (read-line stream nil)
+               (read-line stream nil))
+         (out '()))
+        ((null line) (nreverse out))
+      (with-input-from-string (in line)
+        (read in)                           ; skip first element/column,
+        (setf out (cons (read in) out)))))) ; save second element/column
 
-;(ccls:fit f df d2f time trajectories :guess '(1 1))
+(defun get-trajectories ()
+  "Read in directory, and return an MxN matrix of all M trajectories,
+each with N sampling points, as a foreign array."
+  (grid:make-foreign-array
+   'double-float :initial-contents
+   (mapcar #'get-second-column
+           (directory (concatenate 'string *data-path* "trajectory*")))))
+
+(defvar *trajectories* (get-trajectories) "Foreign MxN array of M trajectories")
+
+
+;;(ls-ice:fit #'f #'df #'d2f time trajectories :guess '(1 1))
--- lisp/test.lisp
+++ lisp/test.lisp
@@ -1,22 +1,82 @@
-; (asdf:operate 'asdf:load-op :gsll)
+;;; My file with test code.
+
+(asdf:operate 'asdf:load-op :gsll)
+
+;; How to look up documentation from GSL doc to GSLL
+(gsl:gsl-lookup "gsl_matrix_get_col") ;; -> GSLL:COLUMN
+(documentation #'GSLL:COLUMN 'function)
+
+;; Here is a 3x3 GSLL array:
+(defvar matrix1 (GRID:MAKE-FOREIGN-ARRAY 'DOUBLE-FLOAT :INITIAL-CONTENTS
+                                        '((-34.5d0 8.24d0 3.29d0)
+                                          (-8.93d0 34.12d0 -6.15d0)
+                                          (49.27d0 -13.49d0 32.5d0))))
+
+(defvar matrix2 (GRID:MAKE-FOREIGN-ARRAY 'DOUBLE-FLOAT :INITIAL-CONTENTS
+                                        '((-43.5d0 4.2d0 1.2d0)
+                                          (-83d0 3.12d0 -6.1d0)
+                                          (94.72d0 31.94d0 3.2d0))))
+
+(grid:dimensions matrix1)
+(grid:gref matrix1 0 0)
+
+
+;;========================================
+
 ; (mapcar (lambda (x) (format t "~s~%" x)) (gsl:examples))
 
-;;; makes matrix m1 zero
-(let ((gsll::m1
-        (grid:make-foreign-array 'double-float :initial-contents
-                                 '((-34.5d0 8.24d0 3.29d0)
-                                   (-8.93d0 34.12d0 -6.15d0)
-                                   (49.27d0 -13.49d0 32.5d0)))))
-   (gsll:set-zero gsll::m1)
-   (grid:copy-to gsll::m1))
+
+;; Why the "copy to"?
+(GRID:COPY-TO (GSLL:MATRIX-TRANSPOSE GSLL::M1 GSLL::M2))
+
+
+;; (in-package antik-user)
+
+;; (setf grid:*default-grid-type* 'grid:foreign-array)
+;; (grid:sin (GRID:MAKE-FOREIGN-ARRAY 'DOUBLE-FLOAT :INITIAL-CONTENTS
+;;                               '((-34.5d0 8.24d0 3.29d0)
+;;                                 (-8.93d0 34.12d0 -6.15d0)
+;;                                 (49.27d0 -13.49d0 32.5d0))))
+
+;; (invert-matrix
+;;    (grid:make-foreign-array
+;;     'double-float :initial-contents '((1.0d0 2.0d0) (3.0d0 4.0d0))))
+
+;; Om(?) Antik:
+;; file:///home/vandelay/quicklisp/dists/quicklisp/software/antik-master-ad6432e3-git/documentation/build/html/gridintro.html
+;; The argument grid-type to a function or the variable
+;; grid:*default-grid-type* should be bound to one of 'cl:array or
+;; 'grid:foreign-array, to specify an array or foreign-array
+;; respectively.
+
+
+
+;; ;;; makes matrix m1 zero
+;; (let ((gsll::m1
+;;         (grid:make-foreign-array 'double-float :initial-contents
+;;                                  '((-34.5d0 8.24d0 3.29d0)
+;;                                    (-8.93d0 34.12d0 -6.15d0)
+;;                                    (49.27d0 -13.49d0 32.5d0)))))
+;;    (gsll:set-zero gsll::m1)
+;;    (grid:copy-to gsll::m1))
+
+
+;; COV
+(LET ((GSLL::V1
+       (GRID:MAKE-FOREIGN-ARRAY 'DOUBLE-FLOAT :INITIAL-CONTENTS
+                                '(-34.5d0 8.24d0 3.29d0)))
+      (GSLL::V2
+       (GRID:MAKE-FOREIGN-ARRAY 'DOUBLE-FLOAT :INITIAL-CONTENTS
+                                '(-8.93d0 34.12d0 -6.15d0))))
+  (LET ((GSLL::MEAN1 (GSLL:MEAN GSLL::V1))
+        (GSLL::MEAN2 (GSLL:MEAN GSLL::V2)))
+    (GSLL:COVARIANCE GSLL::V1 GSLL::V2 GSLL::MEAN1 GSLL::MEAN2)))
 
 ;; GSLL::NONLINEAR-LEAST-SQUARES
 ;; GSLL::LINEAR-LEAST-SQUARES
 ;; GSLL::MINIMIZATION-MULTI
 ;; GSLL::MINIMIZATION-ONE
-;; GSLL:COVARIANCE
-;; GSLL:AUTOCORRELATION
-;; GSLL::CHI-SQUARED
+
 ;; GSLL::EXPONENTIAL-POWER
 ;; GSLL::EXPONENTIAL
 ;; GSLL:CDOT
